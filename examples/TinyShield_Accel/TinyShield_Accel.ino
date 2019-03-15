c32ad6895842690406cb5ce0b34bf1d3d5f0b4f4
info@tinycircuits.com <info@tinycircuits.com>
2019-03-15 19:10:10 -0400

    Add accelerometer stuff

--- examples/TinyShield_Accel/TinyShield_Accel.ino
+++ examples/TinyShield_Accel/TinyShield_Accel.ino
@@ -1,20 +1,23 @@
-/*
-  TinyDuino Accelerometer Demo
-  
-  This example code is in the public domain.
+/*************************************************************************
+ * BMA250 Tutorial:
+ * This example program will show the basic method of printing out the 
+ * accelerometer values from the BMA250 to the Serial Monitor, and the
+ * Serial Plotter
+ * 
+ * Hardware by: TinyCircuits
+ * Code by: Laverena Wienclaw for TinyCircuits
+ *
+ * Initiated: Mon. 11/1/2018
+ * Updated: Tue. 11/2/2018
+ ************************************************************************/
+ 
+#include <Wire.h>         // For I2C communication with sensor
+#include "BMA250.h"       // For interfacing with the accel. sensor
 
-  Written 15 August 2016
-  By 
-  Modified 07 January 2019
-  By Hunter Hykes
-
-  https://TinyCircuits.com
-*/
-
-
-#include <Wire.h>
-#include "BMA250.h"
-
+// Accelerometer sensor variables for the sensor and its values
+BMA250 accel_sensor;
+int x, y, z;
+double temp;
 
 #if defined(ARDUINO_ARCH_SAMD)
 #define SerialMonitorInterface SerialUSB
@@ -22,28 +25,55 @@
 #define SerialMonitorInterface Serial
 #endif
 
-BMA250 accel;
-
-
-void setup()
-{
-  SerialMonitorInterface.begin(9600);
+void setup() {
+  SerialMonitorInterface.begin(115200);
   Wire.begin();
-  accel.begin(BMA250_range_2g, BMA250_update_time_64ms);//This sets up the BMA250 accelerometer
+
+  SerialMonitorInterface.print("Initializing BMA...");
+  // Set up the BMA250 acccelerometer sensor
+  accel_sensor.begin(BMA250_range_2g, BMA250_update_time_64ms); 
 }
 
-void loop()
-{
-  accel.read();//This function gets new data from the accelerometer
-  SerialMonitorInterface.print("X = ");
-  SerialMonitorInterface.print(accel.X);
-  SerialMonitorInterface.print("  ");
-  SerialMonitorInterface.print("Y = ");
-  SerialMonitorInterface.print(accel.Y);
-  SerialMonitorInterface.print("  ");
-  SerialMonitorInterface.print("Z = ");
-  SerialMonitorInterface.print(accel.Z);
-  SerialMonitorInterface.print("  Temperature(C) = ");
-  SerialMonitorInterface.println((accel.rawTemp*0.5)+24.0,1);
-  delay(250);//We'll make sure we're over the 64ms update time set on the BMA250
+void loop() {
+  accel_sensor.read();//This function gets new data from the acccelerometer
+
+  // Get the acceleration values from the sensor and store them into global variables
+  // (Makes reading the rest of the program easier)
+  x = accel_sensor.X;
+  y = accel_sensor.Y;
+  z = accel_sensor.Z;
+  temp = ((accel_sensor.rawTemp * 0.5) + 24.0);
+
+  // If the BMA250 is not found, nor connected correctly, these values will be produced
+  // by the sensor 
+  if (x == -1 && y == -1 && z == -1) {
+    // Print error message to Serial Monitor
+    SerialMonitorInterface.print("ERROR! NO BMA250 DETECTED!");
+  }
+  
+  else { // if we have correct sensor readings: 
+    showSerial();                 //Print to Serial Monitor or Plotter
+  }
+
+  // The BMA250 can only poll new sensor values every 64ms, so this delay
+  // will ensure that we can continue to read values
+  delay(250);
+  // ***Without the delay, there would not be any sensor output*** 
 }
+
+// Prints the sensor values to the Serial Monitor, or Serial Plotter (found under 'Tools')
+void showSerial() {
+  SerialMonitorInterface.print("X = ");
+  SerialMonitorInterface.print(x);
+  
+  SerialMonitorInterface.print("  Y = ");
+  SerialMonitorInterface.print(y);
+  
+  SerialMonitorInterface.print("  Z = ");
+  SerialMonitorInterface.print(z);
+  
+  SerialMonitorInterface.print("  Temperature(C) = ");
+  SerialMonitorInterface.println(temp);
+}
+
+
