c4239c69ac70d8d84739fd3de8661a4bb024184e
Rena <info@tinycircuits.com>
2021-05-24 17:05:26 -0400

    update si7021 code

--- examples/Temperature-Humidity_TinyShield_example/Temperature-Humidity_TinyShield_example.ino
+++ examples/Temperature-Humidity_TinyShield_example/Temperature-Humidity_TinyShield_example.ino
@@ -1,18 +1,15 @@
 /*
   TinyCircuits Si7020 Temperature and Humidity Sensor TinyShield Example Sketch
+
+  This demo shows how to read temperature and humidity data from the Si7020 
+  sensor using the Si7020 library: https://github.com/LowPowerLab/SI7021
+
+  The program will print the temperature & humidity, then enable the heater for 
+  30 seconds and print the temperature & humidity to show the difference the 
+  internal heater can make. The heater will then be disabled with a cool down 
+  period of 30 seconds.
   
-  This demo shows the bare minimum to read temperature and humidity data from 
-  the Si7020 sensor using the Si7020 library written by Marcus Sorensen.
-
-  This example is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  Written 20 March 2016
-  By Ben Rose
-  Modified 07 January 2019
-  By Hunter Hykes
+  Rewritten May 2021 to include built-in heater functionality by L. Wienclaw
 
   https://TinyCircuits.com
 */
@@ -20,34 +17,57 @@
 #include <Wire.h>
 #include <SI7021.h>
 
-#if defined(ARDUINO_ARCH_SAMD)
-#define SerialMonitorInterface SerialUSB
-#else
-#define SerialMonitorInterface Serial
-#endif
-
 SI7021 sensor;
 
-void setup()
-{
+// Make Serial Monitor compatible for all TinyCircuits processors
+#if defined(ARDUINO_ARCH_AVR)
+  #define SerialMonitorInterface Serial
+#elif defined(ARDUINO_ARCH_SAMD)
+  #define SerialMonitorInterface SerialUSB
+#endif
+
+void setup() {
   SerialMonitorInterface.begin(115200);
   Wire.begin();
   SerialMonitorInterface.print("Initializing sensor... ");
   if(!sensor.begin()){
-  	SerialMonitorInterface.println("Sensor not found!");
-  	while(true);
+    SerialMonitorInterface.println("Sensor not found!");
+    while(true);
   }
   SerialMonitorInterface.println("Success!");
+
+  // this driver should work for SI7020 and SI7021, this returns 20 or 21
+  int deviceid = sensor.getDeviceId();
+//    SerialMonitorInterface.println(deviceid);
 }
 
-void loop()
-{
-  int celcius=sensor.getCelsiusHundredths()/100;
-  int relativeHumidity=sensor.getHumidityPercent();
-  SerialMonitorInterface.print(celcius);
-  SerialMonitorInterface.print(" deg Celsius\t");
-  SerialMonitorInterface.print(relativeHumidity);
-  SerialMonitorInterface.println("% relative humidity");
-  
-  delay(500);
+void loop() {
+    // read and print temp and humidity
+    float temperature = sensor.getCelsiusHundredths() / 100.0;
+    SerialMonitorInterface.print(temperature);
+    SerialMonitorInterface.print(" deg Celsius\t");
+    int humidity = sensor.getHumidityPercent();
+    SerialMonitorInterface.print(humidity);
+    SerialMonitorInterface.println("% relative humidity");
+
+    // enable internal heater for testing
+    sensor.setHeater(true);
+    delay(30000);
+    sensor.setHeater(false);
+    
+    // see if heater changed temperature
+    // read and print temp and humidity
+    temperature = sensor.getCelsiusHundredths() / 100.0;
+    SerialMonitorInterface.print(temperature);
+    SerialMonitorInterface.print(" deg Celsius\t");
+    humidity = sensor.getHumidityPercent();
+    SerialMonitorInterface.print(humidity);
+    SerialMonitorInterface.println("% relative humidity -- (after heater on)");
+    
+    //cool down
+    delay(30000);
+
+    // get humidity and temperature in one shot, saves power because sensor takes temperature when doing humidity anyway
+//    si7021_env data = sensor.getHumidityAndTemperature();
+//    delay(5000);
 }
